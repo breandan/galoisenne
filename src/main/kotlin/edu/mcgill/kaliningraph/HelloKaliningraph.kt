41d9a8b16e5298bcd89fe1918378a9258ea8e759
breandan <bre@ndan.co>
2020-04-23 23:59:26 -0400

    add to hello world demo

--- src/main/kotlin/edu/mcgill/kaliningraph/HelloKaliningraph.kt
+++ src/main/kotlin/edu/mcgill/kaliningraph/HelloKaliningraph.kt
@@ -1,5 +1,9 @@
 package edu.mcgill.kaliningraph
 
+import kweb.Kweb
+import kweb.new
+
+@ExperimentalStdlibApi
 fun main() {
   println("Hello Kaliningraph!")
 
@@ -27,6 +31,32 @@ fun main() {
   println("WL3(efgh) = $efgh_wl3")
   println("Isomorphic: ${abcd.isomorphicTo(efgh)}")
 
-  val rg = buildGraph { prefAttach(a.asGraph(), 20) }
-  rg.show()
+  browserDemo()
 }
+
+@ExperimentalStdlibApi
+fun browserDemo() {
+  val graph = mutableListOf(buildGraph { Graph(a) })
+
+  Kweb(port = 16097) {
+    doc.body.apply {
+      val el = new { element("div").innerHTML(graph.last().html()) }
+      on.keydown {
+        when {
+          "Left" in it.key -> {
+            graph.removeLastOrNull()
+            graph.lastOrNull()?.html()?.let(el::innerHTML)
+          }
+          "Right" in it.key -> {
+            graph.add(graph.last().grow())
+            el.innerHTML(graph.last().html())
+          }
+        }
+      }
+    }
+  }
+
+  ProcessBuilder("x-www-browser", "http://0.0.0.0:16097").start()
+}
+
+fun Graph.grow() = buildGraph { prefAttach(this@grow, 1) }
