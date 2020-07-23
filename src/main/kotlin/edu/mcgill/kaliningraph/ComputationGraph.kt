ebeb955756f826816c9105096558f6c270043d3f
breandan <bre@ndan.co>
2020-07-23 04:40:04 -0400

    rename value to slot (holding values)

--- src/main/kotlin/edu/mcgill/kaliningraph/ComputationGraph.kt
+++ src/main/kotlin/edu/mcgill/kaliningraph/ComputationGraph.kt
@@ -3,24 +3,23 @@ package edu.mcgill.kaliningraph
 import kotlin.reflect.KProperty
 
 class Notebook {
-  var graph = Graph<Value, UnlabeledEdge>()
+  var graph = Graph<Slot, UnlabeledEdge>()
 
-  var a by Value(); var b by Value(); var c by Value(); var d by Value()
-  var e by Value(); var f by Value(); var g by Value(); var h by Value()
-  var i by Value(); var j by Value(); var k by Value(); var l by Value()
+  var a by Slot(); var b by Slot(); var c by Slot(); var d by Slot()
+  var e by Slot(); var f by Slot(); var g by Slot(); var h by Slot()
+  var i by Slot(); var j by Slot(); var k by Slot(); var l by Slot()
 
-  operator fun Value.minus(value: Any) =
-    Value("-" + randomString()) {
+  operator fun Slot.minus(value: Any) =
+    Slot("-" + randomString()) {
       setOf(UnlabeledEdge(this), UnlabeledEdge(wrap(value)))
     }.also { graph += Graph(it) }
 
-  operator fun Value.plus(value: Any) =
-    Value("+" + randomString()) {
+  operator fun Slot.plus(value: Any) =
+    Slot("+" + randomString()) {
       setOf(UnlabeledEdge(this), UnlabeledEdge(wrap(value)))
     }.also { graph += Graph(it) }
 
-
-  fun wrap(value: Any) = if(value is Value) value else Value(value.toString())
+  fun wrap(value: Any) = if(value is Slot) value else Slot(value.toString())
 
   companion object {
     operator fun invoke(builder: Notebook.() -> Unit) =
@@ -28,28 +27,28 @@ class Notebook {
   }
 }
 
-class Value(
+class Slot(
   id: String = randomString(),
-  override val edgeMap: (Value) -> Collection<UnlabeledEdge>
-) : Node<Value, UnlabeledEdge>(id) {
-  constructor(id: String? = randomString(), out: Set<Value> = emptySet()) :
+  override val edgeMap: (Slot) -> Collection<UnlabeledEdge>
+) : Node<Slot, UnlabeledEdge>(id) {
+  constructor(id: String? = randomString(), out: Set<Slot> = emptySet()) :
     this(id ?: randomString(), { out.map { UnlabeledEdge(it) } })
 
-  constructor(out: Set<Value> = setOf()) : this(randomString(), { out.map { UnlabeledEdge(it) } })
+  constructor(out: Set<Slot> = setOf()) : this(randomString(), { out.map { UnlabeledEdge(it) } })
 
-  override fun equals(other: Any?) = (other as? Value)?.id == id
+  override fun equals(other: Any?) = (other as? Slot)?.id == id
   override fun hashCode() = id.hashCode()
   override fun toString() = id.first().toString()
-  override fun new(id: String?, out: Set<Value>) = Value(id, out)
-  override fun new(id: String, edgeMap: (Value) -> Collection<UnlabeledEdge>) = Value(id, edgeMap)
+  override fun new(id: String?, out: Set<Slot>) = Slot(id, out)
+  override fun new(id: String, edgeMap: (Slot) -> Collection<UnlabeledEdge>) = Slot(id, edgeMap)
 
-  operator fun setValue(builder: Notebook, prop: KProperty<*>, value: Value) {
-    builder.graph += Graph(Value(prop.name) {
-      setOf(UnlabeledEdge(Value("=" + randomString()) { setOf(UnlabeledEdge(value)) }))
+  operator fun setValue(builder: Notebook, prop: KProperty<*>, value: Slot) {
+    builder.graph += Graph(Slot(prop.name) {
+      setOf(UnlabeledEdge(Slot("=" + randomString()) { setOf(UnlabeledEdge(value)) }))
     })
   }
 }
 
-open class UnlabeledEdge(override val target: Value): Edge<UnlabeledEdge, Value>(target) {
-  override fun newTarget(target: Value) = UnlabeledEdge(target)
+open class UnlabeledEdge(override val target: Slot): Edge<UnlabeledEdge, Slot>(target) {
+  override fun newTarget(target: Slot) = UnlabeledEdge(target)
 }
