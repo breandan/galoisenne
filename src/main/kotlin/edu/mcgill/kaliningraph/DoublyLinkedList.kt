3e50d87f518028d3b55482b347db7239382fd423
breandan <bre@ndan.co>
2020-11-15 05:39:13 -0500

    self-loop on list boundaries

--- src/main/kotlin/edu/mcgill/kaliningraph/DoublyLinkedList.kt
+++ src/main/kotlin/edu/mcgill/kaliningraph/DoublyLinkedList.kt
@@ -3,6 +3,7 @@ package edu.mcgill.kaliningraph
 
 fun main() {
   var head = DLL("a").let { ('c'..'z').fold(it) { a, b -> a + b.toString() } }
+  println(head.size)
   assertDLL(head)
 
   head = head.insert("b")
@@ -12,11 +13,12 @@ fun main() {
 
 private fun assertDLL(head: DLL<String>) =
   (1 until head.size - 1).map { head[it] }.forEach {
-    val isDLLForward = it.succ?.pred === it
-    val isDLLBack = it.pred?.succ === it
+    val isDLLForward = it.succ.pred === it
+    val isDLLBack = it.pred.succ === it
+    val isDLLTwice = it.succ.succ.pred.pred === it // TODO: fixme
     println(it)
-    assert(isDLLForward) { "${it.succ?.pred} != $it" }
-    assert(isDLLBack) { "${it.pred?.succ} != $it" }
+    assert(isDLLForward) { "${it.succ.pred} != $it" }
+    assert(isDLLBack) { "${it.pred.succ} != $it" }
   }
 
 class LL<T>(val head: T, val succ: LL<T>? = null) {
@@ -33,40 +35,40 @@ class LL<T>(val head: T, val succ: LL<T>? = null) {
 
 class DLL<T>(
   val head: T,
-  pred: (DLL<T>) -> DLL<T>? = { null },
-  succ: (DLL<T>) -> DLL<T>? = { null }
+  pred: (DLL<T>) -> DLL<T> = { it },
+  succ: (DLL<T>) -> DLL<T> = { it }
 ) {
-  val pred: DLL<T>? by lazy { pred(this) }
-  val succ: DLL<T>? by lazy { succ(this) }
+  val pred: DLL<T> by lazy { pred(this) }
+  val succ: DLL<T> by lazy { succ(this) }
   val size: Int
-    get() = 1 + (succ?.size ?: 0)
+    get() = 1 + (if (hasNext()) 0 else succ.size)
   val tail: T
-    get() = if(succ == null) head else succ!!.tail
+    get() = if (hasNext()) head else succ.tail
 
   operator fun plus(t: T): DLL<T> =
-    if (succ == null)
-      DLL(head, { it.prepend(pred) }, { me -> DLL(t, { me }, { null }) })
+    if (hasNext())
+      DLL(head, { pred.append(it) }, { me -> DLL(t, { me }, { it }) })
     else
-      DLL(head, { it.prepend(pred) }, { succ?.plus(t)?.prepend(it) })
+      DLL(head, { pred.append(it) }, { it.append(succ + t) })
 
-  fun append(succ: DLL<T>?): DLL<T> =
-    DLL(head, { it.prepend(pred) }, { succ })
+  fun append(succ: DLL<T>): DLL<T> = succ.prepend(this) // TODO: flip?
 
-  fun prepend(pred: DLL<T>?): DLL<T> =
-    DLL(head, { pred }, { succ?.prepend(it) })
+  fun prepend(pred: DLL<T>): DLL<T> =
+    if (hasNext()) DLL(head, { pred }, { it })
+    else DLL(head, { pred }, { succ.prepend(it) })
 
   fun insert(t: T): DLL<T> =
-    if(succ == null) this + t
-    else DLL(t, { it.prepend(pred?.plus(head)) }, { succ?.prepend(it) })
+    if (hasNext()) this + t
+    else DLL(t, { it.prepend(pred + head) }, { succ.prepend(it) })
 
-  operator fun get(i: Int): DLL<T> = if(i == 0) this else succ!![i - 1]
+  operator fun get(i: Int): DLL<T> = if (i == 0) this else succ[i - 1]
 
-  fun reversed(): DLL<T> =
-    if(succ == null) this
-    else DLL(tail, { it.prepend(pred) }, { TODO() })
+//  fun reversed(): DLL<T> =
+//    if (hasNext()) this
+//    else DLL(tail, { pred.append(it) }, { succ.reversed() + head })
 
-  fun last(): T = if (succ == null) head else succ!!.last()
-  fun first() = head
+  override fun toString(): String =
+    if (hasNext()) "[$head]" else "[$head]<->$succ"
 
-  override fun toString(): String = "[$head]<->$succ"
+  fun hasNext() = succ == this
 }
