a5b47097ae4d4cc2fbd9da35e1675d7f9e62dd1c
breandan <bre@ndan.co>
2021-05-16 01:15:27 -0400

    ensure no self loops in the middle

--- src/test/kotlin/edu/mcgill/kaliningraph/experimental/DoublyLinkedListTest.kt
+++ src/test/kotlin/edu/mcgill/kaliningraph/experimental/DoublyLinkedListTest.kt
@@ -23,9 +23,8 @@ class DoublyLinkedListTest {
 
   private fun assertDLL(head: DLL<String>) =
     (1 until head.size - 1).map { head[it] }.forEach {
-      val isDLLForward = it.next.prev === it
-      val isDLLBack = it.prev.next === it
-      println(head)
+      val isDLLForward = it.next.prev === it && it.next !== it
+      val isDLLBack = it.prev.next === it && it.prev !== it
       assertTrue(isDLLForward) { "${it.next.prev} != $it" }
       assertTrue(isDLLBack) { "${it.prev.next} != $it" }
     }
