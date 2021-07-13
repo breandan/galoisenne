42034a73dd706cdbc684ebf9572795cdfbcafb81
breandan <breandan.considine@mail.mcgill.ca>
2021-07-12 22:37:39 -0400

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
   `maven-publish`
-  kotlin("jvm") version "1.6.0-dev-806"
+  kotlin("jvm") version "1.6.0-dev-1004"
   kotlin("jupyter.api") version "0.10.0-106"
   id("com.github.ben-manes.versions") version "0.39.0"
 }
@@ -26,14 +26,12 @@ dependencies {
   implementation(kotlin("reflect"))
 
   // Notebook integration testing
-  // Notebook integration test
   testImplementation(kotlin("reflect"))
   testImplementation(kotlin("scripting-jvm"))
   testImplementation("junit", "junit", "4.13.2")
   testImplementation("org.jetbrains.kotlinx:kotlin-jupyter-kernel:0.10.0-106")
 
   // Property-based testing
-
   val kotestVersion = "4.6.1"
   testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
   testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
