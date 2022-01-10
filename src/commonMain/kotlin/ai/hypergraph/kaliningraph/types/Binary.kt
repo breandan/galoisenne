42bb71e876b43177e995d575ac7f9f3a66acf0cc
breandan <bre@ndan.co>
2022-01-09 21:06:22 -0500

    use shl and document variable naming semantics

--- src/commonMain/kotlin/ai/hypergraph/kaliningraph/types/Binary.kt
+++ src/commonMain/kotlin/ai/hypergraph/kaliningraph/types/Binary.kt
@@ -9,25 +9,29 @@ sealed class B<X, P : B<X, P>>(open val x: X? = null) {
 
   override fun equals(other: Any?) = toString() == other.toString()
   override fun hashCode() = this::class.hashCode() + x.hashCode()
-  override fun toString() = "" + (x ?: "") + if (this is F) "0" else "1"
-  fun toInt() = toString().toIntOrNull(2)
+  override fun toString() = "" + (x ?: "") + if (this is T) "1" else "0"
 }
+
 open class T<X>(override val x: X? = null) : B<X, T<X>>(x) { companion object: T<Nothing>() }
 open class F<X>(override val x: X? = null) : B<X, F<X>>(x) { companion object: F<Nothing>() }
 
-tailrec fun B<*, *>?.toInt(i: Int = 0, j: Int = 1): Int =
-  if (this == null) i else (x as B<*, *>?).toInt(i + if (this is T) j else 0, 2 * j)
+fun B<*, *>.toInt(): Int = toInt(toString())
+tailrec fun toInt(s: String, sum: Int = 0): Int =
+  if (s.isEmpty()) sum else toInt(s.substring(1), (sum shl 1) + s[0].digitToInt())
 
+// Left padded with 0, B_0N indicates a binary string *0{B_0N}
 typealias B_01<B> = T<B_0<B>>
 typealias B_03<B> = T<B_01<B>>
 typealias B_07<B> = T<B_03<B>>
 typealias B_015<B> = T<B_07<B>>
 
+// Exact binary strings, B_N indicates a binary string 2^N-1
 typealias B_1<B> = T<B>
 typealias B_3<B> = T<B_1<B>>
 typealias B_7<B> = T<B_3<B>>
 typealias B_15<B> = T<B_7<B>>
 
+// Exact binary strings, B_N indicates a binary string 2^N
 typealias B_0<B> = F<B>
 typealias B_2<B> = F<B_1<B>>
 typealias B_4<B> = F<B_2<B>>
@@ -35,7 +39,7 @@ typealias B_8<B> = F<B_4<B>>
 typealias B_16<B> = F<B_8<B>>
 
 @JvmName("b0p1") fun B_0<Nothing>.plus1(): B_1<Nothing> = T()
-@JvmName("b1p1") fun B_1<Nothing>.plus1(): B_2<Nothing> = F(T())
+@JvmName("b1p1") fun B_1<Nothing>.plus1(): B_2<Nothing> = F(this)
 @JvmName("b3p1") fun B_3<Nothing>.plus1(): B_4<Nothing> = F(x!!.plus1())
 @JvmName("b7p1") fun B_7<Nothing>.plus1(): B_8<Nothing> = F(x!!.plus1())
 @JvmName("b15p1") fun B_15<Nothing>.plus1(): B_16<Nothing> = F(x!!.plus1())
