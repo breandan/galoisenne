1fdbed1cb7f24b5b3538bc7169ae5d2b12abe959
breandan <bre@ndan.co>
2022-06-27 00:27:39 -0700

    fix eca constructor

--- src/commonMain/kotlin/ai/hypergraph/kaliningraph/automata/ECA.kt
+++ src/commonMain/kotlin/ai/hypergraph/kaliningraph/automata/ECA.kt
@@ -13,9 +13,9 @@ val <A> Context<A>.r get() = π3
 val ecaAlgebra = algebra()
 fun makeVec(len: Int) =
   FreeMatrix(ecaAlgebra, len, 1) { r, c ->
-    if(r == len / 2) Context(null, true, null)
-    else if (r!= len / 2) Context(null, true, null)
-    else null
+    if (r % 2 == 0) Context(null, true, null)
+    else Context(null, false, null)
+//    else null
   }
 
 // Create a tridiagonal matrix
@@ -25,34 +25,35 @@ fun FreeMatrix<Context<Boolean?>?>.genMat(): FreeMatrix<Context<Boolean?>?> =
   }
 
 fun FreeMatrix<Context<Boolean?>?>.print() =
-  println(transpose.map { if(it?.q == true) "1" else " "}.toString())
+//  println(transpose.map { if(it?.q == true) "1" else " "}.toString())
+  println(transpose.map { it?.toList()?.joinToString("") { if (it == null) "?" else if (it) "1" else "0" } ?: "[???]" }.toString())
+
 
 fun Context<Boolean?>.applyRule(
   // https://www.wolframalpha.com/input?i=rule+110
   rule: (Boolean, Boolean, Boolean) -> Boolean = { p, q, r -> (q && !p) || (q xor r) }
 ): Context<Boolean?> =
-  Context(null, rule(p ?: false, q ?: false, r ?: false), null)
+  Context(null, rule(p ?: false, q!!, r ?: false), null)
 
-// Create a tridiagonal matrix
 fun FreeMatrix<Context<Boolean?>?>.nonlinearity() =
   FreeMatrix(numRows, 1) { r, c -> this[r, c]?.applyRule() }
 
 fun algebra() =
   Ring.of<Context<Boolean?>?>(
-  nil = null,
-  times = { a: Context<Boolean?>?, b: Context<Boolean?>? ->
-    if (a == null && b == null) null
-    else if (a != null && b != null)
-      Context(a.π1 ?: b.π1, a.π2 ?: b.π2, a.π3 ?: b.π3)
-    else  a ?: b
-  },
-  plus = { a: Context<Boolean?>?, b: Context<Boolean?>? ->
-    if (a == null && b != null) b
-    else if (a != null && b != null)
-      if (a.π1 == null) Context(b.π2, null, null)
-      else if (a.π2 == null) Context(a.π1, b.π2, null)
-      else Context(a.π1, a.π2, b.π3)
-    else if (a != null && b == null) a
-    else null
-  }
-)
+    nil = null,
+    times = { a: Context<Boolean?>?, b: Context<Boolean?>? ->
+      if (a == null && b == null) null
+      else if (a != null && b != null) Context(null, b.π2, null)
+      else null
+    },
+    plus = { a: Context<Boolean?>?, b: Context<Boolean?>? ->
+//      (a to b).also { println(it) }
+      (if (a == null && b != null) Context(b.π2, null, null)
+      else if (a != null && b != null)
+//        if (a.π1 == null) Context(b.π2, null, null)
+        if (a.π2 == null) Context(a.π1, b.π2, null)
+        else Context(a.π1, a.π2, b.π2)
+      else if (a != null && b == null) a
+      else null)//.also { println("$a + $b = $it") }
+    }
+  )
--- src/commonTest/kotlin/ai/hypergraph/kaliningraph/automata/ECA.kt
+++ src/commonTest/kotlin/ai/hypergraph/kaliningraph/automata/ECA.kt
@@ -3,14 +3,14 @@ package ai.hypergraph.kaliningraph.automata
 import kotlin.test.Test
 
 class ECATest {
-  /*
- ./gradlew jvmTest --tests "ai.hypergraph.kaliningraph.automata.ECATest.testSimpleECA"
- */
+/*
+./gradlew jvmTest --tests "ai.hypergraph.kaliningraph.automata.ECATest.testSimpleECA"
+*/
   @Test
   fun testSimpleECA() {
     val init = makeVec(10)
     val ruleMat = init.genMat()
-    var next = (ruleMat * init).nonlinearity().also { it.print() }
+    var next = (ruleMat * init).also { it.print() }.nonlinearity().also { it.print() }
     next = (ruleMat * init).nonlinearity().also { it.print() }
     next = (ruleMat * init).nonlinearity().also { it.print() }
   }
