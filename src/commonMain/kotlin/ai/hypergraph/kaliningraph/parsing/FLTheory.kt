343dc565abc6e29cca02af1a76260009a7ab5155
breandan <bre@ndan.co>
2022-10-07 00:09:11 -0400

    more FL theory

--- src/commonMain/kotlin/ai/hypergraph/kaliningraph/parsing/FLTheory.kt
+++ src/commonMain/kotlin/ai/hypergraph/kaliningraph/parsing/FLTheory.kt
@@ -10,16 +10,24 @@ val REG.asCFG: CFG by cache { map { (a, b) -> a to listOf(b) }.toSet() }
 
 data class REL(val reg: REG) // https://en.wikipedia.org/wiki/Regular_language#Closure_properties
 data class CFL(val cfg: CFG) // https://en.wikipedia.org/wiki/Context-free_language#Closure_properties
+// Not actually context-sensitive, but close enough for now.
 // May need to stage set expressions to support both ∪ and ∩,
 // but currently just represents the intersection of CFL(s).
-class Decidable(vararg val cfls: CFL) // https://en.wikipedia.org/wiki/Recursive_language#Closure_properties
+// https://en.wikipedia.org/wiki/Context-sensitive_grammar#Closure_properties
+class CSL(vararg cfls: CFL) { val cfls: Array<CFL> = cfls.toSet().toTypedArray() }
 
-operator fun CFL.contains(s: String): Boolean = cfg.parse(s) != null
+// REL ⊂ CFL ⊂ CSL
 operator fun REL.contains(s: String): Boolean = s in reg.asCFG.language
+operator fun CFL.contains(s: String): Boolean = cfg.parse(s) != null
+operator fun CSL.contains(s: String): Boolean = cfls.all { s in it }
 
 // https://arxiv.org/pdf/2209.06809.pdf
 // http://www.cs.umd.edu/~gasarch/BLOGPAPERS/cfg.pdf
-infix fun CFL.intersect(rel: REL): CFL = TODO("Implement Bar-Hillel")
+infix fun CFL.intersect(rel: REL): CFL = TODO("Implement Bar-Hillel construction")
+infix fun REL.intersect(cfl: CFL): CFL = cfl intersect this
+
 // https://sites.cs.ucsb.edu/~cappello/136/lectures/17cfls/slides.pdf#page=9
-infix fun CFL.intersect(cfl: CFL): Decidable = Decidable(this, cfl)
-infix fun Decidable.intersect(cfl: CFL): Decidable = Decidable(*cfls, cfl)
+infix fun CFL.intersect(cfl: CFL): CSL = CSL(this, cfl)
+infix fun CSL.intersect(cfl: CFL): CSL = CSL(*cfls, cfl)
+infix fun CFL.intersect(csl: CSL): CSL = CSL(this, *csl.cfls)
+infix fun CSL.intersect(csl: CSL): CSL = CSL(*cfls, *csl.cfls)
