8b8a356f2189aea303d6c85f2cf70803d7a7a04d
breandan <bre@ndan.co>
2023-02-09 12:58:46 -0500

    remove rootProject.name and use @sellmair's workaround

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
-rootProject.name = "kaliningraph"
+// Look out for: https://youtrack.jetbrains.com/issue/KT-56536/Multiplatform-Composite-build-fails-on-included-build-with-rootProject.name-buildIdentifier.name
+//rootProject.name = "kaliningraph"
 
 pluginManagement.repositories {
   gradlePluginPortal()
