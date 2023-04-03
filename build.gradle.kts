02379394871a4d92900e8a9053e8ed662dbe4dfe
breandan <bre@ndan.co>
2023-04-03 01:01:13 -0400

    versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ kotlin {
         implementation(kotlin("stdlib-common"))
         implementation(kotlin("reflect"))
 
-        implementation("org.jetbrains.kotlinx:kotlinx-html:0.8.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-html:0.8.0")
       }
     }
 
