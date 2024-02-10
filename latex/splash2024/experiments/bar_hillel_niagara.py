9bf1f6caa89b93912513e907ef982ab3ea9c80a5
breandan <bre@ndan.co>
2024-02-09 20:01:31 -0500

    plot sample efficiency

--- latex/splash2024/experiments/bar_hillel_niagara.py
+++ latex/splash2024/experiments/bar_hillel_niagara.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import tikzplotlib
 
 # Load the data into a pandas DataFrame
 data = pd.read_csv('bar_hillel_niagara.csv', sep=r'\s*,\s*')
 
+max_samples = data['samples'].max() + 1
+print(f"Max samples: {max_samples}")
 # Create buckets for the 'samples' column in increments of 5
-buckets = np.arange(0, data['samples'].max() + 1, 1)
+buckets = np.arange(0, max_samples, 100)
 bucket_labels = buckets[1:]  # Label each bucket by its upper limit
 
 # Initialize a DataFrame to hold the percentage of instances per bucket and level
@@ -14,21 +17,24 @@ percentages = pd.DataFrame(columns=bucket_labels)
 
 # Calculate percentages for each 'lev'
 for lev in sorted(data['lev'].unique()):
+    print("Calculating percentages for lev =", lev)
     lev_data = data[data['lev'] == lev]
     counts, _ = np.histogram(lev_data['samples'], bins=buckets)
     cum_counts = np.cumsum(counts) / len(lev_data) * 100  # Calculate cumulative percentage
     percentages.loc[lev] = cum_counts
 
+print('Done calculating percentages')
 
 # Plotting
 fig, ax = plt.subplots(figsize=(10, 6))
 
 # Plot a separate bar for each 'lev'
 for lev in percentages.index:
-    ax.bar(np.arange(len(percentages.columns)), percentages.loc[lev], 1.0, label=f"$\Delta_L(\sigma, \sigma')={lev}$")
+    print(lev)
+    ax.bar(np.arange(len(percentages.columns)), percentages.loc[lev], 1.0, label=f"$\Delta_L={lev}$")
 
-ax.set_xlabel('Number of samples drawn before finding human repair (log scale)')
-ax.set_ylabel('Percent of instances matching human repair in $\leq N$ samples')
+ax.set_xlabel('Samples drawn (log scale)')
+ax.set_ylabel('% matching human repair in $\leq N$ samples')
 ax.set_title('Sample Efficiency of Levenshtein-Bar-Hillel Sampler on StackOverflow Repairs $\Delta_L(\sigma, \sigma\')\in[1, 3]$')
 
 # Calculate tick spacing to only show about ten or so ticks across the x-axis
@@ -46,10 +52,10 @@ ax.set_xscale('log')
 ax.set_xticklabels(tick_labels)  # Optional rotation for better label readability
 
 # Put the legend in the bottom right corner
-ax.legend()
+# ax.legend()
 
 plt.tight_layout()
 
 # Show the plot
-plt.show()
-
+# plt.show()
+tikzplotlib.save("sample_efficiency.tex")
