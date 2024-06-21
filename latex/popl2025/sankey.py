d5eaa093fe1af8456635f6b5855f2bbed46e618f
breandan <bre@ndan.co>
2024-06-21 14:49:01 -0400

    separate splash camera ready from popl

---
+++ latex/popl2025/sankey.py
@@ -0,0 +1,17 @@
+from matplotlib.sankey import Sankey
+import matplotlib.pyplot as plt
+import tikzplotlib
+
+fig = plt.figure(figsize = [10,10])
+ax = fig.add_subplot(1,1,1)
+
+Sankey(ax=ax,  flows = [967,-485,-238,-153,-4,-87],
+       labels = ['Snippets', 'Top-1', 'Top-2+', 'NoGen', 'NoRec', 'Error'],
+       orientations = [0, -1, -1, -1, -1, -1],
+       scale=1/2500, trunklength=1,
+       edgecolor = '#099368', facecolor = '#099368'
+       ).finish()
+plt.axis("off")
+# plt.show()
+
+tikzplotlib.save("sankey.tex")
