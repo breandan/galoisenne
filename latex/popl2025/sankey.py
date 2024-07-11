89d1565adb1075e4c3ff157010c4e313c7a0193a
breandan <bre@ndan.co>
2024-07-11 02:04:49 -0400

    update sankey plot from latest niagara run

--- latex/popl2025/sankey.py
+++ latex/popl2025/sankey.py
@@ -5,9 +5,9 @@ import tikzplotlib
 fig = plt.figure(figsize = [10,10])
 ax = fig.add_subplot(1,1,1)
 
-Sankey(ax=ax,  flows = [967,-485,-238,-153,-4,-87],
-       labels = ['Snippets', 'Top-1', 'Top-2+', 'NoGen', 'NoRec', 'Error'],
-       orientations = [0, -1, -1, -1, -1, -1],
+Sankey(ax=ax,  flows = [2247,-622,-475,-751,-253,-45,-101],
+       labels = ['Snippets', 'Top-1', 'Top-[2-10]', 'Top-11+', 'NoGen', 'NoRec', 'Error'],
+       orientations = [0, -1, -1, -1, -1, -1, -1],
        scale=1/2500, trunklength=1,
        edgecolor = '#099368', facecolor = '#099368'
        ).finish()
