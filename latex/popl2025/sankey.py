e1ab0e68beccfaa68e4dcf86b4dcc280975ce35a
breandan <bre@ndan.co>
2024-11-18 23:42:44 -0500

    Redraw Sankey plot

--- latex/popl2025/sankey.py
+++ latex/popl2025/sankey.py
@@ -5,13 +5,14 @@ import tikzplotlib
 fig = plt.figure(figsize = [10,10])
 ax = fig.add_subplot(1,1,1)
 
-Sankey(ax=ax,  flows = [2247,-622,-475,-751,-253,-45,-101],
-       labels = ['Snippets', 'Top-1', 'Top-[2-10]', 'Top-11+', 'NoGen', 'NoRec', 'Error'],
-       orientations = [0, -1, -1, -1, -1, -1, -1],
-       scale=1/2500, trunklength=1,
-       edgecolor = '#099368', facecolor = '#099368'
-       ).finish()
+Sankey(
+  ax=ax, flows = [5136,-1725,-737,-730,-1646,-108,-48,-142],
+  labels = ['Total', 'Top-1', 'Top-[2-10]', 'Top-11-99', 'Top-100+', 'NG', 'NR', 'OOM'],
+  orientations = [0, -1, -1, -1, -1, -1, -1, -1],
+  scale= 1/2500, trunklength=0.5,
+  edgecolor = '#099368', facecolor = '#099368'
+).finish()
 plt.axis("off")
-# plt.show()
+plt.show()
 
-tikzplotlib.save("sankey.tex")
+# tikzplotlib.save("sankey.tex")
