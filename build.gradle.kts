994d409d487d46e5c13aafc220d0a0cd4650596b
breandan <bre@ndan.co>
2025-02-16 05:20:57 -0500

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
   `maven-publish`
   kotlin("multiplatform") version "2.1.0"
 //  kotlin("jupyter.api") version "0.11.0-225"
-  id("com.github.ben-manes.versions") version "0.51.0"
+  id("com.github.ben-manes.versions") version "0.52.0"
   id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
