c1dc14fc4dc7a7b6d1d2038ab212901aa2c9397b
breandan <bre@ndan.co>
2025-02-21 02:22:15 -0500

    string predicate encoding

--- src/commonMain/kotlin/ai/hypergraph/kaliningraph/automata/Nominal.kt
+++ src/commonMain/kotlin/ai/hypergraph/kaliningraph/automata/Nominal.kt
@@ -3,7 +3,12 @@ package ai.hypergraph.kaliningraph.automata
 import ai.hypergraph.kaliningraph.parsing.Σᐩ
 import ai.hypergraph.kaliningraph.types.*
 
-typealias StrPred = (Σᐩ) -> Bln
+data class StrPred(val arg: Σᐩ): (Σᐩ) -> Boolean {
+  override operator fun invoke(s: Σᐩ) =
+    if (arg == "[.*]") true
+    else if (arg.startsWith("[!=]")) s != arg.drop(4)
+    else s == arg
+}
 
 // https://arxiv.org/pdf/1402.0897.pdf
 // https://cs.ru.nl/~freek/courses/mfocs-2021/slides/steven.pdf
@@ -14,19 +19,12 @@ class NOM(override val Q: TSA, override val init: Set<Σᐩ>, override val final
     Q.map { it.second }.filter { it.startsWith("[!=]") }.toSet()
   }
 
-  fun Σᐩ.predicate(): (Σᐩ) -> Boolean =
-    if (this == "[.*]") { s: Σᐩ -> true }
-    else if (startsWith("[!=]")) { s: Σᐩ -> s != drop(4) }
-    else { s: Σᐩ -> s == this }
-
   val mapF: Map<Σᐩ, List<Π2<StrPred, Σᐩ>>> by lazy {
-    Q.map { q -> q.first to q.second.predicate() to q.third }.groupBy { it.first }
-      .mapValues { (_, v) -> v.map { it.second to it.third } }
+    Q.map { q -> Triple(q.first, StrPred(q.second), q.third) }.groupBy { it.first }
+      .mapValues { (_, v) -> v.map { Pair(it.second, it.third) } }
   }
 
-  val flattenedTriples: Set<Triple<Σᐩ, StrPred, Σᐩ>> by lazy {
-    Q.map { (a, b, c) -> a to b.predicate() to c }.toSet()
-  }
+  val flattenedTriples: Set<Triple<Σᐩ, StrPred, Σᐩ>> by lazy { Q.map { (a, b, c) -> a to StrPred(b) to c }.toSet() }
 
   override fun recognizes(str: List<Σᐩ>): Boolean =
     str.fold(init) { acc, sym ->
