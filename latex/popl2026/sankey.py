d06ccc5ab0aca3e2677b2f043b2221af6ca0f0f2
breandan <bre@ndan.co>
2025-07-07 18:15:26 -0400

    polish stackoverflow eval

--- latex/popl2026/sankey.py
+++ latex/popl2026/sankey.py
@@ -6,7 +6,7 @@ fig = plt.figure(figsize = [10,10])
 ax = fig.add_subplot(1,1,1)
 
 Sankey(
-  ax=ax, flows = [2211,-677,-344,-97,-377,-716],
+  ax=ax, flows = [2238,-675,-567,-104,-285,-607],
   labels = ['Total', 'Top-1', 'Top-[2-10]', 'Top-11-99', 'Top-100+', 'NR'],
   orientations = [0, 1, 1, 1, 1, -1],
   scale= 1/3500, trunklength=1,
