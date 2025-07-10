43ac327d9552b8a8c4a8c0c64e32b0959136574d
breandan <bre@ndan.co>
2025-07-09 23:26:03 -0400

    Bar-Hillel in the acyclic case <=> CFL reachability

--- latex/popl2026/sankey.py
+++ latex/popl2026/sankey.py
@@ -10,6 +10,8 @@ Sankey(
   labels = ['Total', 'Top-1', 'Top-[2-10]', 'Top-11-99', 'Top-100+', 'NR'],
   orientations = [0, 1, 1, 1, 1, -1],
   scale= 1/3500, trunklength=1,
+  shoulder=0.05,
+  # radius=0.1,
   edgecolor = '#099368', facecolor = '#099368'
 ).finish()
 plt.axis("off")
