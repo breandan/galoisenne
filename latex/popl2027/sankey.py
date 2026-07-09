b6d205c6f49938523a6f89937a04d68bd1b51c20
breandan <bre@ndan.co>
2026-07-08 20:26:09 -0400

    more related work

--- latex/popl2027/sankey.py
+++ latex/popl2027/sankey.py
@@ -10,7 +10,8 @@ Sankey(
   labels = ['Total', 'Top-1', 'Top-[2-10]', 'Top-11-99', 'Top-100+', 'NR'],
   orientations = [0, 1, 1, 1, 1, -1],
   scale= 1/3500, trunklength=1,
-  shoulder=0.05,
+  shoulder=0.06,
+  head_angle=95,
   # radius=0.1,
   edgecolor = '#099368', facecolor = '#099368'
 ).finish()
